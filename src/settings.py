90026eddc0b9a3fb549594fc7d8c023017115a3a
erastov-alex <erastov.alex@gmail.com>
2023-10-26 18:11:01 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "CADI-AI"
 PROJECT_NAME_FULL: str = "CADI-AI: Cashew Disease Identification with AI"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
